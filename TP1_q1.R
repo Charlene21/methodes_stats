0d8dfde6a3fe47a5650028fa3411a5f53d609fa9
gononl <lucas.gonon@ensimag.grenoble-inp.fr>
2017-02-16 16:47:28 +0100

    Modification GetSubset question 4

--- TP1_q1.R
+++ TP1_q1.R
@@ -1,20 +1,8 @@
 #TP1
 
 load("RentDJINDUS.RData")
-
+source("fonctions.R")
 #Question 1  Etudiez la distribution empirique des rentabilités journalières, hebdomadaires, mensuelles de votre titre. Interprétez les résultats.
-
-GetSubset <- function(date_debut, date_fin){
-  vect = c()
-  for (i in 1:length(RentJ$Dates)){
-    if(RentJ$Dates[i]<= date_fin && RentJ$Dates[i]>= date_debut){
-      vect = c(vect,RentJ$Rt[i])
-    }
-    i= i+1;
-  }
- return(vect)
-} 
-
 plot(RentJ$Rt, type='l', col='blue')
 
 #Rentabilités journalières
--- q4.R
+++ q4.R
@@ -1,46 +1,54 @@
 #espérance des rentabilités journalières sur [1990-2002] et [2003-2016]
 .libPaths("/user/1/taram/Public/RLib") 
 library(stringr)
-mean1 = 0;
-mean2 = 0;
-i = 0;
-j = 0;
-for (i in 1:length(RentJ$Dates)){
-	if(str_sub(RentJ$Dates[i], 1, 4) <= 2002){
-		mean1 = mean1 + RentJ$Rt[i];
-		i= i+1;
-	}
-	else {
-		mean2 = mean2 + RentJ$Rt[i];
-		j=j+1;
-		}
-				}
-cat("mean 1 = ", mean1/i, "\n")
-cat("mean 2 = ", mean2/j, "\n")
+# mean1 = 0;
+# mean2 = 0;
+# i = 0;
+# j = 0;
+# for (i in 1:length(RentJ$Dates)){
+# 	if(str_sub(RentJ$Dates[i], 1, 4) <= 2002){
+# 		mean1 = mean1 + RentJ$Rt[i];
+# 		i= i+1;
+# 	}
+# 	else {
+# 		mean2 = mean2 + RentJ$Rt[i];
+# 		j=j+1;
+# 		}
+# }
+# cat("mean 1 = ", mean1/i, "\n")
+# cat("mean 2 = ", mean2/j, "\n")
 
 #Variance des rentabilités journalières sur [1990-2002] et [2003-2016]
+# var1 = 0;
+# n1=0;
+# var2 = 0;
+# n2=0;
+# for (i in 1:length(RentJ$Dates)){
+# 	if(str_sub(RentJ$Dates[i], 1, 4) <= 2002){
+# 		n1 = n1 + 1;
+# 		var1 = var1 + (RentJ$Rt[i] - mean1)^2;
+# 	}
+# 	else {
+# 		n2 = n2 + 1
+# 		var2 = var2 + (RentJ$Rt[i] - mean2)^2;
+# 		}
+# 				}
+# 
+# var1 = var1/n1;
+# var2 = var2/n2;
+# cat("var1 = ",var1, "\n");
+# cat("var2 = ",var2, "\n");
 
-var1 = 0;
-n1=0;
-var2 = 0;
-n2=0;
-for (i in 1:length(RentJ$Dates)){
-	if(str_sub(RentJ$Dates[i], 1, 4) <= 2002){
-		n1 = n1 + 1;
-		var1 = var1 + (RentJ$Rt[i] - mean1)^2;
-	}
-	else {
-		n2 = n2 + 1
-		var2 = var2 + (RentJ$Rt[i] - mean2)^2;
-		}
-				}
-
-var1 = var1/n1;
-var2 = var2/n2;
-
-
-cat("var1 = ",var1, "\n");
-cat("var2 = ",var2, "\n");
-
-
+#### Correction avec la fonction GetSubset
+source("fonctions.R")
+vect1 = GetSubset("1990-01-01", "2002-12-31");
+vect2 = GetSubset("2003-01-01", "2016-12-31");
+mean1 = mean(vect1);
+mean2 = mean(vect2);
+cat("mean 1 = ", mean1, "\n");
+cat("mean 2 = ", mean2, "\n");
+var1 = var(vect1);
+var2 = var(vect2);
+cat("var1 = ", var1, "\n");
+cat("var2 = ", var2, "\n");
 

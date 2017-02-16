82d44a00549c93f797716c915ac6bb7aa758b7d0
Charlène Paviot <charlene_2106@hotmail.fr>
2017-02-16 18:01:37 +0100

    ajouts questions 1 2 et 6

--- TP1_q2_b.R
+++ TP1_q2_b.R
@@ -19,6 +19,33 @@ print(summary(armaDowJones))
 armaDowJones<-arma(RentJ$Rt, order=c(2,2)) #AIC = -67814.24
 print(summary(armaDowJones))
 
+armaDowJones<-arma(RentJ$Rt, order=c(0,2)) #AIC = -67814.31 <<<<<<<<<<<<
+print(summary(armaDowJones))
+
+armaDowJones<-arma(RentJ$Rt, order=c(2,0)) #AIC = -67813.48
+print(summary(armaDowJones))
+
+armaDowJones<-arma(RentJ$Rt, order=c(1,1)) #AIC = -67794.18
+print(summary(armaDowJones))
+
+armaDowJones<-arma(RentJ$Rt, order=c(1,0)) #AIC = -67796.16
+print(summary(armaDowJones))
+
+armaDowJones<-arma(RentJ$Rt, order=c(0,1)) #AIC = -67796.15
+print(summary(armaDowJones))
+
+armaDowJones<-arma(RentJ$Rt, order=c(0,3)) #AIC = -67811.65
+print(summary(armaDowJones))
+
+armaDowJones<-arma(RentJ$Rt, order=c(3,0)) #AIC = -67810.97
+print(summary(armaDowJones))
+
+armaDowJones<-arma(RentJ$Rt, order=c(1,2)) #AIC = -67813.01
+print(summary(armaDowJones))
+
+armaDowJones<-arma(RentJ$Rt, order=c(2,1)) #AIC = -67812.72
+print(summary(armaDowJones))
+
 #meilleur modele : arma(2,2)
 
 cat("variance totale : ", var(RentJ$Rt), "\n")
